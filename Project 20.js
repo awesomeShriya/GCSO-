f216a19c0a16d76ebfbe120cffcbf64dc980091d
awesomeShriya <68244315+awesomeShriya@users.noreply.github.com>
2020-07-17 10:13:42 -0500

    Add files via upload

---
+++ Project 20.js
@@ -0,0 +1,29 @@
+var car,wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=randoom(400,1500);
+  car=createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,60, eight/2);
+  car.veloctiyX = speed;
+}
+
+function draw() {
+  background(255,255,255); 
+  if (wall.x-car < (carr.width+wall.width/2)) {
+    car.veloctiyX=0;
+    var deformation=0.5 * weight * speed* speed/22509;
+    if(deformation>180){
+      car.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColor=color(230,230,0);
+    }
+    if(deformation<100){
+      car.shapeColor=color(0,255,0);
+    }
+  }
+  drawSprites();
+}
